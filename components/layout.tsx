34d423ed263501201752e7d52b675d88d93184e2
b-hash <brianmhayashi@gmail.com>
2020-12-28 11:17:49 -0800

    (add) bhash logo favicon

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,11 @@ export default function Layout({ children }) {
 	return (
 		<div className={styles.container}>
 			<Head>
-				<link rel="icon" href="" />
+				<link rel="icon" href="/bhash_logo.ico" />
+				<meta
+					name="description"
+					content="B-Hash's software engineering portfolio"
+				/>
 			</Head>
 			<main>{children}</main>
 		</div>
